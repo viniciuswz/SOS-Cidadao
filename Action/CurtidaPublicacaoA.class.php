ea32263bade212e493df946ac6ec0d05bf6c1b94
Bezerra <albuquerque.b.gabriel@gmail.com>
2018-07-26 17:56:46 -0300

    select do Usuario da publicacao

--- Action/CurtidaPublicacaoA.class.php
+++ Action/CurtidaPublicacaoA.class.php
@@ -5,6 +5,10 @@ use Model\CurtidaPublicacaoM;
 class CurtidaPublicacaoA extends CurtidaPublicacaoM{
     private $sqlSelect = "SELECT status_publi_curti FROM publicacao_curtida WHERE cod_usu = '%s' AND cod_publi = '%s'";
 
+    private $sqlUpdate = "UPDATE publicacao_curtida SET status_publi_curti = '%s',
+                                                        ind_visu_dono_publi = '%s' WHERE cod_usu = '%s' AND cod_publi='%s'";
+    
+    private $selectCodUsu ="SELECT cod_usu FROM publicacao WHERE cod_publi = '%s'";
     public function select(){
      echo $sql = sprintf($this->sqlSelect,
                      $this->getCodUsu(),
