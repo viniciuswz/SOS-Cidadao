8af86f8430ef2edcd4d205dc689d7451707b97a8
Bezerra <albuquerque.b.gabriel@gmail.com>
2018-07-26 16:20:10 -0300

    Sprintf da função select

--- Action/CurtidaPublicacaoA.class.php
+++ Action/CurtidaPublicacaoA.class.php
@@ -3,5 +3,11 @@ namespace Action;
 use Model\CurtidaPublicacaoM;
 
 class CurtidaPublicacaoA extends CurtidaPublicacaoM{
-    private $sqlSelect = "SELECT status_publi_curti FROM publicacao_curtida WHERE cod_usu = '%s' AND cod_publi = '%s";
+    private $sqlSelect = "SELECT status_publi_curti FROM publicacao_curtida WHERE cod_usu = '%s' AND cod_publi = '%s"
+
+    public function select(){
+      $sql = sprintf($this->sqlSelect,
+                     $this->getCodUsu(),
+                     $this->getCodPubli());
+    }
 }
