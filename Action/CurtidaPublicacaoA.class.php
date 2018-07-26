f84269d77d4c53ccab86424a6bd8f7de58fc979f
Bezerra <albuquerque.b.gabriel@gmail.com>
2018-07-26 16:09:14 -0300

    Select do Usuário na Publicação

--- Action/CurtidaPublicacaoA.class.php
+++ Action/CurtidaPublicacaoA.class.php
@@ -3,5 +3,5 @@ namespace Action;
 use Model\CurtidaPublicacaoM;
 
 class CurtidaPublicacaoA extends CurtidaPublicacaoM{
-    
+    private $sqlSelect = "SELECT status_publi_curti FROM publicacao_curtida WHERE cod_usu = '%s' AND cod_publi = '%s";
 }
