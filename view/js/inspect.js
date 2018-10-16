4fb35eb1edf78d347054cc04f39a2b12c7ad9970
viniciuswz <32021203+viniciuswz@users.noreply.github.com>
2018-10-16 16:38:02 -0300

    colocando gemidao

--- view/js/inspect.js
+++ view/js/inspect.js
@@ -1,57 +1,74 @@
 
-    document.addEventListener('keydown', function() {
-        if (event.keyCode == 123) {
-          alert("SAI DAQUI SEU LADRÃO");
-          return false;
-        } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-          alert("SAI DAQUI SEU LADRÃO");
-          return false;
-        } else if (event.ctrlKey && event.keyCode == 85) {
-          alert("SAI DAQUI SEU LADRÃO");
-          return false;
-        }
-      }, false);
-      
-      if (document.addEventListener) {
-        document.addEventListener('contextmenu', function(e) {
-          alert("SAI DAQUI SEU LADRÃO");
-          e.preventDefault();
-        }, false);
-      } else {
-        document.attachEvent('oncontextmenu', function() {
-          alert("SAI DAQUI SEU LADRÃO");
-          window.event.returnValue = false;
-        });
-      }
-
-
-ou isso
-
-
+$(document).ready(function(){
+  $('body').attr("id","ok");
+  var isPlaying = false;
+  function togglePlay(){
+    if (isPlaying) {
+      myAudio.play();
+    } else {
+      myAudio.play();
+    }
+    alert("PARA DE OLHAR CODIGO DOS OUTROS")
+  };
+  function verificar(){
+    var id =  $('body').attr("id");
+    if(id == 'ok'){
+      $('body').attr("id","tentou");
+      $('body').html("<audio id='myAudio' controls  loop style='display:block'><source src='grrr.wav' type='audio/mp4'></source></audio>");
+      var myAudio = document.getElementById("myAudio");
+      myAudio.currentTime = 0.1;
+      togglePlay()
+    }else{
+      // myAudio.onplaying = function() {
+      //   isPlaying = true;
+      // };
+      // myAudio.onpause = function() {
+      //   isPlaying = false;
+      // };
+    }
+  }
+  // myAudio.onplaying = function() {
+  //   isPlaying = true;
+  // };
+  // myAudio.onpause = function() {
+  //   isPlaying = false;
+  // };
   document.onkeydown = function(e) {
     if(event.keyCode == 123) {
-       return false;
+      verificar();
+      return false;
     }
     if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-       return false;
+      verificar();
+      return false;
     }
     if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-       return false;
+      verificar();
+      return false;
     }
     if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-       return false;
+      verificar();
+      return false;
     }
     if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-       return false;
-    }if (document.addEventListener) {
-        document.addEventListener('contextmenu', function(e) {
-          alert("SAI DAQUI SEU LADRÃO");
-          e.preventDefault();
-        }, false);
-      } else {
-        document.attachEvent('oncontextmenu', function() {
-          alert("SAI DAQUI SEU LADRÃO");
-          window.event.returnValue = false;
-        });
-      }
+      verificar();
+      return false;
+    }
   }
+  
+  if (document.addEventListener) {
+    document.addEventListener('contextmenu', function(e) {
+      verificar();
+      //alert("SAI DAQUI SEU LADRÃO");
+      e.preventDefault();
+    }, false);
+  } else {
+    document.attachEvent('oncontextmenu', function() {
+      // alert("SAI DAQUI SEU LADRÃO");
+      verificar();
+      window.event.returnValue = false;
+    });
+  }
+  // var banana = document.getElementById("trolei");
+
+})
