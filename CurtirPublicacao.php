b8d020aea8db7c539572c9298fa1c812c96db6ff
viniciuswz <32021203+viniciuswz@users.noreply.github.com>
2018-10-23 10:38:39 -0300

    atualizacao

--- CurtirPublicacao.php
+++ CurtirPublicacao.php
@@ -4,6 +4,7 @@ require_once(SITE_ROOT.DS.'autoload.php');
 use Core\Usuario;
 use Core\CurtidaPublicacao;
 use Classes\ValidarCampos;
+use Core\Publicacao;
 session_start();
 try{
     $tipoUsuPermi = array('Comum','Funcionario','Prefeitura','Moderador','Adm');
@@ -12,17 +13,16 @@ try{
     
     $nomesCampos = array('ID');// Nomes dos campos que receberei da url, ID = da publicacao
     $validar = new ValidarCampos($nomesCampos, $_GET);//Verificar se eles existem, se nao existir estoura um erro
+    $publi = new Publicacao();
     $validar->verificarTipoInt($nomesCampos, $_GET); 
 
     $curtidaPub = new CurtidaPublicacao();
     $curtidaPub->setCodUsu($_SESSION['id_user']);
     $curtidaPub->setCodPubli($_GET['ID']);
     $curtidaPub->select();    
-    if(isset($_GET['pagina'])){ //Ta curtindo atraves da listagem de todas as publicaçoess
-        echo "<script> javascript:window.location='view/todasreclamacoes.php?pagina=".$_GET['pagina']."';</script>";
-    }else{//Ta curtindo atreves da pagina de uma publicacoes especifica
-        echo "<script> javascript:window.location='view/reclamacao.php?ID=".$_GET['ID']."';</script>";
-    }        
+    $ja = $publi->getQuantCurtidas($_GET['ID']);   
+    
+    echo $ja;  
 
 }catch(Exception $exc){
     $erro = $exc->getCode();   
