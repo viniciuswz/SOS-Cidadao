d2523015e8473455f19e0476ea2574139bc60e02
Daniel52x <32081901+Daniel52x@users.noreply.github.com>
2018-10-24 12:20:39 -0300

    aprimorado

--- view/js/rolagemChat.js
+++ view/js/rolagemChat.js
@@ -1,10 +1,10 @@
 
-
+var paginacao = 0
 
 setTimeout(function(){
 
     $(document).ready(function(){
-    
+
 
 
         //$//("#pa").scrollTop($("#pa")[0].scrollHeight);
@@ -17,17 +17,79 @@ setTimeout(function(){
          
            
         
-              
+               paginacao--;
                //Simulate server delay;
                setTimeout(function(){
-                   // Hide loader on success
-                   //$('#loader').hide();
-                   // Reset scroll
+                
+
+                $.ajax({
+                    
+                    url: '../PegarMensagens.php',
+                    type: "get",
+                    data: "pagina="+paginacao+"&ID=30",
+                    success: function(data){            
+                        //$('#lista').html(data);
+                        //$("#pa").prepend(data); 
+                        teste3(data);
+                    }
+                            
+            
+                }); 
+
                    $('#pa').scrollTop(30);
                    alert("ta no topo")
                },780); 
            }
         });
         
-        }) 
+        })
+        
+        
 },1000); 
+
+function teste3(resposta){
+    var arr1 = JSON.parse(resposta);   
+    
+    document.getElementById('ImgDeba').innerHTML = "<img  src='../Img/debate/"+arr1[0]['dadosDeba'][0]['img_deba']+"'>";   
+    var contador;
+    mensa2 = "";
+    for(contador=0; contador < arr1[2]['debateQParticipa'].length; contador++){
+        mensa2 += "<div class=contatinhos><a href='debate_mensagens.php?ID="+arr1[2]['debateQParticipa'][contador]['cod_deba']+"&pagina=ultima'><div class=img-debate><img src='../Img/debate/"+arr1[2]['debateQParticipa'][contador]['img_deba']+"' alt=debate></div></a><div class=status-debate><p>"+arr1[2]['debateQParticipa'][contador]['nome_deba']+"</p></div><div class=info-contatinho><div class=data_mensagem><p>"+arr1[2]['debateQParticipa'][contador]['dataHora_deba']+"</p></div>";
+        if(arr1[2]['debateQParticipa'][contador]['quantidade'] > 0 ) {
+            mensa2 +="<div class=qtd_mensagens><p>"+arr1[2]['debateQParticipa'][contador]['quantidade']+"</p></div>";
+        }
+        mensa2 += "</div></div>"
+    }
+    mensa = ""; 
+    
+    for(contador=0; contador < arr1[3]['mensagens'].length; contador++){
+        classe = arr1[3]['mensagens'][contador]['classe'];
+        var msg_id = arr1[3]['mensagens'][contador]['cod_mensa'];
+
+            mensa += "<div class=" + classe + ">";
+        
+        if(classe == 'linha-mensagem_padrao'){
+            ultimo_id = msg_id;
+            mensa += "<div class=usuario-msg-foto><img src='../Img/perfil/"+arr1[3]['mensagens'][contador]['img_perfil_usu']+"'></div><div class=mensagem_padrao><span class=nome><a href=perfil_reclamacao.php?ID="+arr1[3]['mensagens'][contador]['cod_usu']+">"+arr1[3]['mensagens'][contador]['nome_usu']+"</a></span>";
+        }else{
+            mensa += "<div>";
+        }
+            mensa += "<span>"+arr1[3]['mensagens'][contador]['texto_mensa'];
+        if(classe == 'linha-mensagem_sistema') { 
+            ultimo_id = msg_id;
+            mensa += "<span>"+arr1[3]['mensagens'][contador]['hora']+"</span>"
+        }else{
+            ultimo_id = msg_id;
+            mensa += "<sub>"+arr1[3]['mensagens'][contador]['hora']+"</sub>";
+        }
+            mensa += "</span></div></div>";
+        }
+            
+         //alert(ultimo_id)  
+    
+    
+    //document.getElementsByClassName('mensagens').innerHTML = "";    
+    document.getElementById('contatosJs').innerHTML = mensa2;
+   // document.getElementById('pa').innerHTML = mensa;
+   $("#pa").prepend(mensa);
+}
