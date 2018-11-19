4ba437e2e12be46fe57b0114c20b2f19a3d12fbf
viniciuswz <vinicius-araujo0@hotmail.com>
2018-11-19 21:01:40 -0200

    a

--- Db/scripts-basico.sql
+++ Db/scripts-basico.sql
@@ -13,14 +13,14 @@ INSERT INTO `reclama1`.`tipo_usuario` (`descri_tipo_usu`) VALUES ('Sistema');
 
 
 
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Saúde','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Acessibilidade','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Mobilidade Urbana','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Cultura, Esporte e Lazer','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Educação','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Iluminação Pública','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Lixo e Poluição','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Obras Pública','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Segurança','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Meio Ambiente','A')
-INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Poluição sonora','A')
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Saúde','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Acessibilidade','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Mobilidade Urbana','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Cultura, Esporte e Lazer','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Educação','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Iluminação Pública','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Lixo e Poluição','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Obras Pública','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Segurança','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Meio Ambiente','A');
+INSERT INTO `categoria`(`cod_cate`, `descri_cate`, `status_cate`) VALUES (NULL,'Poluição sonora','A');
